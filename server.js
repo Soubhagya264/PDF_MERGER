e1e8da7dcaf27e2fdaa6c04a0d5e8a9b37135021
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2022-11-18 13:14:22 +0530

    Mergerfile

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const app=express()
+const express=require('express');
+const app = express();
+const path=require('path');
+
+const multer=require('multer');
+const upload=multer({dest:"uploads/"})
+
+const {mergepdf}=require("./merge.js");
+
 const port=5000
 app.get('/', (req, res) =>{
-
+    res.sendFile(path.join(__dirname,"template/index.html"));
 })
 
 app.listen(port,()=>{
-    consol.log("Server started")
+    console.log("Server started on port",port);
+})
+
+app.use('/static',express.static('public'));
+
+app.post('/merge', upload.array('pdfs',2),async (req, res,next)=>{
+    console.log(req.files);
+    await mergepdf(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path));
+    res.redirect(
+        "http://localhost:5000/static/merge.pdf"
+    )
+  
 })
